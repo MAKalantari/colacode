cf7cb0590207c7e144ab744805a7eda10126b819
MAKalantari <mk3000ss@gmail.com>
2025-07-21 03:04:36 +0330

    Initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+
+## Installation
+Download it from release Page
+
+
+    
+## Features
+
+- Free and Open-Source
+- No integration of Telemetry Spywares.
+- Theming. using the power of css and html styling, ColaCode can look modern, classic, minimal, fancy or whatever you like it to look like!
+- Cross platform
+- Lightweight and Bloatfree
+- Syntax Highlighting
+- Code completion
+- Support for variaty of languages
+- Out of the box features (makes the need for extensions obselete)
+- Easy on the eye and suitable for those who are visually impaired
+- focused on stability
+- No paid features and Ai promotions, No inaccurate inline code "completion", No view blocking panels.
+
+
+## Support
+
+For support, email makalantari98@gmail.com or join our telegram channel.
+
+
+
+## License
+
+- Copyright (c) ColaCode. All rights reserved.
+- ColaCode is licensed under [AGPL v3.0 (GNU AFFERO GENERAL PUBLIC LICENSE)](https://github.com/MAKalantari/coloacode?tab=AGPL-3.0-1-ov-file)
+
+## Disclaimer
+- Naming: Both "Cola" and "Code" are completely generic nouns. "Cola" and "Code" takes no influance from trademarked brand/product names that are similiar to any combination of the two words.
+
+## Contributing
+Contributions are always welcome!
+
+
---
+++ src/js/main.js
@@ -0,0 +1,233 @@
+const contextmenu = document.getElementById("contextmenu");
+const resizeNotif = document.getElementById("resizeNotif");
+var customCursor = false;
+var grid = true;
+var grid_steps = 10;
+var fixed_grid = true
+var grid_amount = 10
+
+
+addEventListener("contextmenu", (e) => {
+    e.preventDefault();
+    contextmenu.style.display = "flex";
+    contextmenu.style.left = e.clientX + "px";
+    contextmenu.style.top = e.clientY + "px";
+});
+
+var lastElementClicked = null;
+var doubleClickTimeout = 250;
+var justClicked = false;
+var doubleClickTimer = null;
+
+addEventListener("click", (e) => {
+    if (e.target != contextmenu) {
+        contextmenu.style.display = "none";
+    }
+    lastElementClicked = e.target;
+    if (justClicked && e.target == lastElementClicked) {
+        justClicked = false;
+        onDoubleClick(e);
+    } else {
+        justClicked = true;
+        if (doubleClickTimer != null)
+            clearTimeout(doubleClickTimer);
+        doubleClickTimer = setTimeout(()=>{
+            justClicked = false;
+        }, doubleClickTimeout);
+    }
+});
+
+function onDoubleClick(e) {
+    if (e.target.className == "edge-top" || e.target.className == "edge-bottom"){
+        e.target.parentNode.style.height = "100%";
+    } else if (e.target.className == "edge-right"){
+        if (e.target.parentNode.parentNode) {
+            var resizing = e.target.parentNode;
+            var resizingBound = resizing.getBoundingClientRect();
+            var parent = resizing.parentNode;
+            var parentBound = parent.getBoundingClientRect();
+            var resizingLocalX = resizingBound.left - parentBound.left;
+            resizing.style.width = ((parentBound.width - resizingLocalX) / parentBound.width) * 100 + "%";
+
+        }
+        else
+            e.target.parentNode.style.width = "100%";
+    }
+}
+
+var isResizing = false;
+var resizingElement = null;
+var resizingElement_starting_bound;
+var resizing_right;
+var resizing_left;
+var resizing_top;
+var resizing_bottom;
+var resizingEdge = null;
+var resizingElement_siblings_on_right = [];
+var resizingElement_siblings_on_right_total_width = 0;
+var resizingElement_siblings_on_left = [];
+var resizingElement_siblings_on_left_total_width = 0;
+
+
+addEventListener("mousedown", (e) => {
+    if (e.target.className == "edge-bottom") {
+        e.preventDefault();
+        e.stopPropagation();
+        isResizing = true;
+        resizeNotif.style.display = "flex";
+        resizingElement = e.target.parentNode;
+        resizingElement_starting_bound = resizingElement.getBoundingClientRect();
+        resizingEdge = e.target;
+        resizing_bottom = true;
+    } else if (e.target.className == "edge-right") {
+        e.preventDefault();
+        e.stopPropagation();
+        isResizing = true;
+        resizeNotif.style.display = "flex";
+        resizingElement = e.target.parentNode;
+        resizingElement_starting_bound = resizingElement.getBoundingClientRect();
+        resizingEdge = e.target;
+        resizing_right = true;
+
+        resizingElement_siblings_on_right = [];
+        resizingElement_siblings_on_right_total_width = 0;
+        var siblings = Array.from(resizingElement.parentNode.children);
+        var resizingElementIndex = siblings.indexOf(resizingElement);
+        for (var i = resizingElementIndex + 1; i < siblings.length; i++) {
+            if (siblings[i].getBoundingClientRect().top == resizingElement.getBoundingClientRect().top) {
+                let sibling_width = siblings[i].getBoundingClientRect().width
+                resizingElement_siblings_on_right.push([siblings[i], sibling_width]);
+                resizingElement_siblings_on_right_total_width += siblings[i].getBoundingClientRect().width;
+            }
+        }
+    } else if (e.target.className == "edge-top") {
+        e.preventDefault();
+        e.stopPropagation();
+        isResizing = true;
+        resizeNotif.style.display = "flex";
+        resizingElement = e.target.parentNode;
+        resizingElement_starting_bound = resizingElement.getBoundingClientRect();
+        resizingEdge = e.target;
+        resizing_top = true;
+    } else if (e.target.className == "edge-left") {
+        e.preventDefault();
+        e.stopPropagation();
+        isResizing = true;
+        resizeNotif.style.display = "flex";
+        resizingElement = e.target.parentNode;
+        resizingElement_starting_bound = resizingElement.getBoundingClientRect();
+        resizingEdge = e.target;
+        resizing_left = true;
+        resizingElement_siblings_on_left = [];
+        var siblings = parentNode.childNodes();
+        var resizingElementIndex = siblings.find(resizingElement);
+        for (var i = 0; i < resizingElementIndex-1; i++) {
+            if (siblings[i].getBoundingClientRect().clientY == resizingElement.getBoundingClientRect()/clientY)
+                resizingElement_siblings_on_left.push([siblings[i], siblings[i].getBoundingClientRect().width]);
+        }
+    }
+});
+
+addEventListener("mouseup", (e) => {
+    isResizing = false;
+    resizeNotif.style.display = "none";
+    resizing_right = false;
+    resizing_left = false;
+    resizing_top = false;
+    resizing_bottom = false;
+});
+
+function round(number, increment, offset) {
+    return Math.ceil((number - offset) / increment ) * increment + offset;
+}
+
+var lastTargetTagName = "none"
+addEventListener("mousemove", (e) => {
+    document.documentElement.style.setProperty("--mouse-x", e.clientX + "px");
+    document.documentElement.style.setProperty("--mouse-y", e.clientY + "px");
+    if (isResizing) {
+        e.preventDefault();
+        e.stopPropagation();
+        var parentBound = resizingElement.parentNode.getBoundingClientRect();
+        var selfBound = resizingElement.getBoundingClientRect();
+        resizeNotif.innerText = Math.round(selfBound.width) + "x" + Math.round(selfBound.height) + "px";
+        
+        var grid_size = {x: grid_amount, y: grid_amount}
+        var to_percentage = resizingEdge.hasAttribute("percent");
+        var unit = to_percentage ? "%" : "px";
+        var value = {x: selfBound.x, y: selfBound.y};
+
+        if (!fixed_grid)
+            grid_size = {
+                x: (parentBound.width)/ grid_steps,
+                y: (parentBound.height) / grid_steps
+            }
+
+        if (resizing_right) {
+            value.x = round(e.clientX - selfBound.left, grid_size.x, 10);
+            var remaining_width = resizingElement_siblings_on_right_total_width - (value.x - resizingElement_starting_bound.width);
+            
+            var sibling_width_multiplier = remaining_width / resizingElement_siblings_on_right_total_width;
+
+            resizingElement_siblings_on_right.forEach(sibling_data => {
+                // sibling_data[0].style.width = sibling_data[1] * sibling_width_multiplier + "px";
+                sibling_data[0].style.width = ((sibling_data[1] / parentBound.width) * sibling_width_multiplier) * 100 + "%"
+            });
+        }
+        else if (resizing_left)
+            value.x = round(resizingElement.style.width = selfBound.right - e.clientX, grid_size.x, 10);
+        if (resizing_bottom)
+            value.y = round(resizingElement.style.height = e.clientY - selfBound.top, grid_size.y, 10);
+        else if (resizing_top)
+            value.y = round(resizingElement.style.height = selfBound.bottom - e.clientY, grid_size.y, 10);
+        
+        if (to_percentage)
+            value = {
+                x: (value.x / parentBound.width) * 100,
+                y: (value.y / parentBound.height) * 100
+            };
+
+        if (resizing_left || resizing_right)
+            resizingElement.style.width = value.x + unit
+        if (resizing_top || resizing_bottom)
+            resizingElement.style.height = value.y + unit
+
+    }
+    else if (e.target.tagName != lastTargetTagName) {
+        lastTargetTagName = e.target.tagName
+        if (customCursor) {
+            if (e.target.tagName == "BUTTON" || e.target.tagName == "LI" ) {
+                document.documentElement.style.setProperty("--cursor-width", "calc(var(--ui-font-size)*.33)")
+                document.documentElement.style.setProperty("--cursor-height", "calc(var(--ui-font-size)*.33)")
+            }
+            else if (e.target.tagName == "TEXTAREA") {
+                document.documentElement.style.setProperty("--cursor-width", "calc(var(--ui-font-size)*.175)")
+                document.documentElement.style.setProperty("--cursor-height", "calc(var(--ui-font-size)*1.33)")
+            }
+            else {
+                document.documentElement.style.setProperty("--cursor-width", "calc(var(--ui-font-size)*.66)")
+                document.documentElement.style.setProperty("--cursor-height", "var(--ui-font-size)")
+            }
+        }
+    }
+});
+
+var all_text_areas = document.getElementsByClassName("code");
+for (var i = 0; i < all_text_areas.length; i++) {
+    const area = all_text_areas[i]
+    area.addEventListener("input", (e) => {
+        var lines = area.value.split(/\r|\r\n|\n/);
+        var lineCount = lines.length;
+        var nums = area.parentNode.getElementsByClassName("num-bar")[0]
+        var linesTillSelection = area.value.substr(0, area.selectionStart).split("\n");
+        var currentLine = linesTillSelection[linesTillSelection.length - 1];
+        var currentLineIndex = linesTillSelection.length - 1
+        
+        if (lineCount.length != nums.childNodes.length) {
+            var newNums = ""
+            for (n = 0; n < lineCount; n++)
+                newNums += "<li>";
+            nums.innerHTML = newNums;
+        }
+    })
+}
